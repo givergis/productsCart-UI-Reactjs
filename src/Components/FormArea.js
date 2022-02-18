bff50043c71c8f285e8371146985e0810f4a309a
givergis <pkgivergis@gmail.com>
2022-02-18 10:03:04 +0530

    validation completed

--- src/Components/FormArea.js
+++ src/Components/FormArea.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect,useRef} from 'react';
 import FormContent from './FormContent';
 import { useForm } from "react-hook-form";
+import {useHistory} from 'react-router-dom';
 
 function FormArea(props) {
 let {input,setInput,item,setItem} = props;
@@ -14,18 +15,19 @@ let {input,setInput,item,setItem} = props;
     //  }
     const { register,handleSubmit, formState: { errors } } = useForm();
      const onSubmit = (event)=>{
-      // event.preventDefault();
       let newItem = item;
       newItem.push(input);
       setItem(newItem);
       setInput({name:'',email:'',number:'',address:''});
        }
 
-     const onHandleClick =()=>{
+       const history = useHistory(); 
+    const onHandleClick =()=>{
       console.log("deleted")
       setItem([]);
+      window.location.reload();
     }
- 
+
     
   return <div>
         <div className='formArea' >
@@ -34,7 +36,7 @@ let {input,setInput,item,setItem} = props;
             <div className="d-flex flex-row align-items-center mb-4">
               <i className="fas fa-user fa-lg me-3 fa-fw"></i>
               <div className="form-outline flex-fill mb-0">
-                <input type="text" {...register("name",{ required: true })} name="name"  value={input.name} onChange={ event=>setInput({...input,name:event.target.value})} id="form3Example1c" className="form-control" required />
+                <input type="text" {...register("name",{ required: true,minLength: 5 })} name="name"  value={input.name} onChange={ event=>setInput({...input,name:event.target.value})} id="form3Example1c" className="form-control" required />
                 <label className="form-label" for="form3Example1c">Your Name  <span className='error'>{errors.name && <i>: Please check the Name</i>}</span></label>
               </div>
              
@@ -43,7 +45,7 @@ let {input,setInput,item,setItem} = props;
             <div className="d-flex flex-row align-items-center mb-4">
               <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
               <div className="form-outline flex-fill mb-0">
-                <input type="email"  {...register("email",{ required: true })} name="email" value={input.email}  onChange={event=>setInput({...input,email:event.target.value})} id="form3Example3c" className="form-control" required/>
+                <input type="email"  {...register("email",{ required: true,pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} name="email" value={input.email}  onChange={event=>setInput({...input,email:event.target.value})} id="form3Example3c" className="form-control" required/>
                 <label className="form-label" for="form3Example3c">Your Email  <span className='error'>{errors.email && <i>: Please check the email</i>}</span></label>
               </div>
             </div>
@@ -51,7 +53,7 @@ let {input,setInput,item,setItem} = props;
             <div className="d-flex flex-row align-items-center mb-4">
               <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
               <div className="form-outline flex-fill mb-0">
-                <input type="text"  {...register("number",{ required: true })} name="number" value={input.number}  onChange={event=>setInput({...input,number:event.target.value})} id="form3Example4c" className="form-control" required/>
+                <input type="text"  {...register("number",{ required: true,pattern:/^\d{10}$/,minLength:10,maxLength:10 })} name="number" value={input.number}  onChange={event=>setInput({...input,number:event.target.value})} id="form3Example4c" className="form-control" required/>
                 <label className="form-label" for="form3Example4c">Number  <span className='error'> {errors.number && <i>: Please check the number</i>}</span></label>
               </div>
             </div>
